c385a52f57859c05580eb76afaaa5c02dd735a8e
lbhtran <lbhtran@gmail.com>
2022-12-06 14:46:12 +0000

    docs: add docstring to decorator class

--- lognament/decorators/lognament.py
+++ lognament/decorators/lognament.py
@@ -1,3 +1,7 @@
+"""
+Lognament decorator module.
+"""
+
 import functools
 import time
 from typing import Any, Callable
@@ -6,18 +10,42 @@ from lognament.managers.message_manager import MessageManager
 
 
 class Lognament:
+    """
+    Lognament decorator class
+
+    Can be used to decorate a function direcly
+
+    Attributes
+    ----------
+    fn : Callable
+        function being decorated
+    msg_manager :
+    """
+
     def __init__(self, fn: Callable) -> None:
         functools.update_wrapper(self, fn)
         self.fn = fn
         self.msg_manager = MessageManager(self.fn)
 
     def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        """
+        self :
+
+         : Any
+
+         : Any
+
+
+        Returns
+        -------
+        Any
+
+        """
         start_time = time.perf_counter()
         self.msg_manager.function_start()
         self.msg_manager.function_name()
         try:
             result = self.fn(*args, **kwargs)
-
             return result
         finally:
             end_time = time.perf_counter()
